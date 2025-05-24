0a39ad0641114c97e0a0396bc4eff1aee982d7d4
davecodelab <117858573+davecodelab@users.noreply.github.com>
2025-05-24 22:45:05 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
+import checker from "vite-plugin-checker";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -11,8 +11,11 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    checker({
+      typescript:{
+        enabled:
+          process.env.NODE_ENV==='development', //Only enable in development
+        buildMode:true,
   ].filter(Boolean),
   resolve: {
     alias: {
